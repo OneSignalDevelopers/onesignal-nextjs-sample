686a810ac4045d229db6d6e8929b03ea9e4a3b23
William Shepherd <williamrshepherd@gmail.com>
2023-03-29 11:58:01 -0500

    Generate PWA assets on build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withNextPwa = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+})
+
+const nextConfig = withNextPwa({
   reactStrictMode: true,
-}
+})
 
 module.exports = nextConfig

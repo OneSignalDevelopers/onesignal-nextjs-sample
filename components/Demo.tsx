c194fba9fa37a06ebdc2f823775acc1b4aca647e
William Shepherd <williamrshepherd@gmail.com>
2023-05-18 10:25:11 -0500

    Collocate OneSignal functionality into single component

---
+++ components/Demo.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { sendUserNotification } from "@/common/actions"
+import useOneSignal from "@/hooks/onesignal"
+
+export default function Demo() {
+  const { userId } = useOneSignal()
+
+  return (
+    <>
+      <p className="justify-center mx-auto">
+        OneSignal User ID: {userId || "Anonymous User"}
+      </p>
+
+      <button
+        onClick={(e) => {
+          e.preventDefault()
+          userId && void sendUserNotification(userId)
+        }}
+        className="p-2 border border-slate-50 w-48 justify-center mx-auto hover:border-red-500"
+      >
+        Send notification...
+      </button>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
+import Demo from "@/components/Demo"
 import Head from "next/head"
-import Script from "next/script"
-import useOneSignal from "./hooks/onesignal"
-
 
 export default function Home() {
-  const { user } = useOneSignal()
-
   return (
     <>
       <Head>
@@ -13,22 +9,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Script type="module" src="../scripts/a2hs.js" />
+      {/* <Script type="module" src="../scripts/a2hs.js" /> */}
       <main className="container mx-auto sm:px-6 lg:px-8 mt-4 mb-4 h-screen flex flex-col gap-4">
         <h1 className="text-3xl text-center">OneSignal Next.js 13</h1>
 
-        <pre className="justify-center mx-auto">
-          UserId: {user || "Anonymous"}
-        </pre>
-        <button
-          onClick={(e) => {
-            e.preventDefault()
-            user && void sendUserNotification(user)
-          }}
-          className="p-2 border border-slate-50 w-48 justify-center mx-auto hover:border-red-500"
-        >
-          Send notification...
-        </button>
+        <Demo />
       </main>
     </>
   )
